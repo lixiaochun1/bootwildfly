e8fc7ba4be991496d56d4c0abd0d0ebf837b0077
lixiaochun1 <rec_lee@163.com>
2016-11-06 10:47:22 +0800

    init

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,12 +1,17 @@
 package bootwildfly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
-
-
+	
+	@RequestMapping( value="/**", method= RequestMethod.GET)
+    public String sayBoot(){
+        return ("Hello, map all reuqest to return say boot");
+    }
+	
     @RequestMapping("hello")
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
