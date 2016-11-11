88ad41668f1322563eb4d42860788963837c3e78
lixiaochun1 <rec_lee@163.com>
2016-11-11 16:44:10 +0800

    proxy

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,19 +1,32 @@
 package bootwildfly;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWildFlyController {
 	
+	private RestTemplate restTemplate;
+	
 	@RequestMapping( value="/**", method= RequestMethod.GET)
-    public String sayBoot(){
+    public String sayBoot(HttpServletRequest request){
         return ("Hello, map all reuqest to return say boot");
+    //    return getRestTemplate().getForObject(request.getRequestURL().toString(), String.class);
     }
 	
     @RequestMapping("hello")
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
+    }
+    
+    private RestTemplate getRestTemplate(){
+        if(restTemplate==null){
+            restTemplate= new RestTemplate();
+        }
+        return restTemplate;
     }
 }
